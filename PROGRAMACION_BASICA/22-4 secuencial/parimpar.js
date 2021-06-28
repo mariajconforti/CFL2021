fc026e73b7b49e9197ee3991a6ce962aeb02aa8b
MARIA JOSE <mariajoseconforti@gmail.com>
2021-06-28 20:03:39 -0300

    https://github.com/mariajconforti/CFL2021.git

---
+++ PROGRAMACION_BASICA/22-4 secuencial/parimpar.js
@@ -0,0 +1,10 @@
+console.log("es par impar");
+let numero =readlineSync.questionInt("ingrese un  numero");
+if(numero = 0)
+console.log("ha ingresado el  valor 0");
+else
+  if (numero=int(numero/2)*2)
+    console.log("es par");
+  else
+    console.log ("es impar");
+    
